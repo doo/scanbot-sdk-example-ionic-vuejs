961aeff44245e3ddabdc2c8405b67ae4b5ce9e63
Chandu Ranwala <54301535+ranwala@users.noreply.github.com>
2023-12-19 11:51:25 +0100

    medical certificate result page eaded

--- src/views/features/data_detectors/MedicalCertificateResultPage.vue
+++ src/views/features/data_detectors/MedicalCertificateResultPage.vue
@@ -2,95 +2,99 @@
     <ion-page>
         <ion-header>
             <ion-toolbar color="primary">
-                <ion-buttons>
+                <ion-buttons slot="start">
                     <ion-back-button></ion-back-button>
                 </ion-buttons>
                 <ion-title> Medical Certificate Result </ion-title>
             </ion-toolbar>
         </ion-header>
         <ion-content class="ion-padding" color="light">
-            <ion-card>
-                <ion-img v-bind:src="imageURL" />
-            </ion-card>
-
-            <ion-label>Form Type: </ion-label>
-
             <ion-list :inset="true">
-                <ion-list-header>
-                    <ion-label>Patient Data</ion-label>
-                </ion-list-header>
-                <ion-item>
-                    <ion-label>
-                        <h3>Insurance Provider</h3>
-                        <p></p>
-                        <!-- <h2>Insurance Provider: {{ medicalCertifacteResult?.patientData?.insuranceProvider }}</h2>
-                    <h3>Name: {{ medicalCertifacteResult?.patientData?.firstName + " " +
-                        medicalCertifacteResult?.patientData?.lastName }}</h3>
-                    <h3>HINR: {{ medicalCertifacteResult?.patientData?.healthInsuranceNumber }}</h3>
-                    <h3>IPNR: {{ medicalCertifacteResult?.patientData?.insuredPersonNumber }}</h3> -->
-                    </ion-label>
-                </ion-item>
-                <ion-item>
-                    <ion-label>Mega Man X</ion-label>
-                </ion-item>
-                <ion-item>
-                    <ion-label>The Legend of Zelda</ion-label>
-                </ion-item>
-                <ion-item>
-                    <ion-label>Pac-Man</ion-label>
-                </ion-item>
-                <ion-item>
-                    <ion-label>Super Mario World</ion-label>
-                </ion-item>
-            </ion-list>
+                <ion-item-group v-for="medItem in medicalCertifacteResult" :key="medItem.title">
+                    <ion-list-header color="primary">
+                        <ion-label>{{ medItem.title }}</ion-label>
+                    </ion-list-header>
 
-            <!-- <h2>Dates Data:</h2>
-				<ion-label>
-					<h3>Incapable of work since: {{ medicalCertifacteResult?.dates?.incapableOfWorkSince?.dateString ?? "Unknown date" }}</h3>
-					<h3>Incapable of work until: {{ medicalCertifacteResult?.dates?.incapableOfWorkUntil?.dateString ?? "Unknown date" }}</h3>
-					<h3>Diagnosed on: {{ medicalCertifacteResult?.dates?.diagnosedOn?.dateString ?? "Unknown date" }}</h3>
-					<h3>Document Date: {{ medicalCertifacteResult?.dates?.documentDate?.dateString ?? "Unknown date" }}</h3>
-				</ion-label>
-				<h2>Checkbox Data:</h2>
-				<ion-label>
-					<h3>Initial Certificate: {{ (medicalCertifacteResult?.checkboxes?.initialCertificate?.isChecked ?? false) ? "Yes" : "No" }}</h3>
-					<h3>Renewed Certificate: {{ (medicalCertifacteResult?.checkboxes?.renewedCertificate?.isChecked ?? false) ? "Yes" : "No" }}</h3>
-					<h3>Work Accident: {{ (medicalCertifacteResult?.checkboxes?.workAccident?.isChecked ?? false) ? "Yes" : "No" }}</h3>
-					<h3>Accident: {{ (medicalCertifacteResult?.checkboxes?.accident?.isChecked ?? false) ? "Yes" : "No" }}</h3>
-				</ion-label> -->
+                    <ion-item v-for="item in medItem.items" :key="item.key">
+                        <ion-label>
+                            <h3>{{ item.key }}</h3>
+                            <p>{{ item.value }}</p>
+                        </ion-label>
+                    </ion-item>
+                </ion-item-group>
+            </ion-list>
         </ion-content>
     </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, onIonViewDidEnter, IonImg, IonCard, IonItem, IonLabel, IonList, IonListHeader } from '@ionic/vue';
-import { ref } from 'vue';
-
-import { MedicalCertificateScannerResult } from 'capacitor-plugin-scanbot-sdk';
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItemGroup, IonItem, IonLabel, IonList, IonListHeader, onIonViewWillEnter } from '@ionic/vue';
 import { DataDetectorRepository } from '@/utils/data_detector_repository';
-import { ScanbotSDKService } from '@/services/scanbot-service';
-import { stringLiteral } from '@babel/types';
 
-let imageURL = ref<string>();
 let medicalCertifacteResult: { title: string; items: { key: string; value: string; }[]; }[] = [];
 
-onIonViewDidEnter(async () => {
-    // medicalCertifacteResult = DataDetectorRepository.MedResult;
-    // imageURL.value = await ScanbotSDKService.getImageData(medicalCertifacteResult.imageFileUri!);
-    // alert(JSON.stringify(medicalCertifacteResult));
-    // alert(medicalCertifacteResult.patientData.insuranceProvider);
-    // alert(medicalCertifacteResult.patientData.firstName);
-
-    
-
-    medicalCertifacteResult = [
-        {
-            title: 'General Data',
-            items: [
-                { key: '', value: "Scan Document" },
-                { key: '', value: "View Image Results" },
-            ],
-        },
-    ]
+onIonViewWillEnter(async () => {
+    const medData = DataDetectorRepository.MedResult;
+    if (!isObjectEmpty(medData.formType)) {
+        medicalCertifacteResult.push(
+            {
+                title: 'General Information',
+                items: [
+                    { key: 'FormType', value: medData.formType },
+                    { key: 'Captured Image', value: medData.imageFileUri! }
+                ]
+            }
+        )
+    }
+    if (!isObjectEmpty(medData.patientData)) {
+        medicalCertifacteResult.push(
+            {
+                title: 'Patient Data',
+                items: [
+                    { key: 'Insuarance Provider', value: medData.patientData.insuranceProvider! },
+                    { key: 'Full Name', value: medData.patientData.firstName! + medData.patientData.lastName! },
+                    { key: 'Address', value: medData.patientData.address1! + medData.patientData.address2! },
+                    { key: 'Diagnose', value: medData.patientData.diagnose! },
+                ]
+            }
+        )
+    }
+    if (!isObjectEmpty(DataDetectorRepository.MedResult.dates)) {
+        medicalCertifacteResult.push(
+            {
+                title: 'Dates',
+                items: [
+                    { key: 'Date of Birth', value: DataDetectorRepository.MedResult.dates.birthDate?.dateString! },
+                    { key: 'Diagnose On', value: DataDetectorRepository.MedResult.dates.diagnosedOn?.dateString! },
+                    { key: 'Document Date', value: DataDetectorRepository.MedResult.dates.documentDate?.dateString! },
+                    { key: 'Incapable of work since', value: DataDetectorRepository.MedResult.dates.incapableOfWorkSince?.dateString! },
+                    { key: 'Incapable of work until', value: DataDetectorRepository.MedResult.dates.incapableOfWorkUntil?.dateString! },
+                ]
+            }
+        )
+    }
+    if (!isObjectEmpty(DataDetectorRepository.MedResult.checkboxes)) {
+        medicalCertifacteResult.push(
+            {
+                title: 'CheckBoxes',
+                items: [
+                    { key: 'Accident Number', value: DataDetectorRepository.MedResult.checkboxes.accidentNo?.isChecked ? 'True' : 'False' },
+                    { key: 'Accident', value: DataDetectorRepository.MedResult.checkboxes.accident?.isChecked ? 'True' : 'False' },
+                    { key: 'Assigned to Doctor', value: DataDetectorRepository.MedResult.checkboxes.assignedToAccidentInsuranceDoctor?.isChecked ? 'True' : 'False' },
+                    { key: 'Work Accident', value: DataDetectorRepository.MedResult.checkboxes.workAccident?.isChecked ? 'True' : 'False' },
+                    { key: 'Insured Pay Case', value: DataDetectorRepository.MedResult.checkboxes.insuredPayCase?.isChecked ? 'True' : 'False' },
+                ]
+            }
+        )
+    }
 });
+
+/** check the provided object is null */
+const isObjectEmpty = (objectName: any) => {
+    return (
+        objectName &&
+        Object.keys(objectName).length === 0 &&
+        objectName.constructor === Object
+    );
+};
 </script>
